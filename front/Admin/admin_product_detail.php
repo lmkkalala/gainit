25dd2431d7e1d8de15653bd9047dfd7dc45ee6f8
Lucien Kalala <murhulakalalalucien14@gmail.com>
2023-01-17 22:31:18 +0100

    names and text modification for the site

--- front/Admin/admin_product_detail.php
+++ front/Admin/admin_product_detail.php
@@ -312,7 +312,7 @@
 
         <footer class="bg-white sticky-footer">
             <div class="container my-auto">
-                <div class="text-center my-auto copyright"><span>Copyright © MDC 2021</span></div>
+                <div class="text-center my-auto copyright"><span>Copyright © Gain IT</span></div>
             </div>
         </footer>
     </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a></div>
